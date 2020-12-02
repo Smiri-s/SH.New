b398b026af39337bcf8bbd049eccda381bea6648
Smiri-s <stela.hasambelliu@finetuning.it>
2020-12-02 16:10:26 +0100

    Implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,13 @@ module ApplicationHelper
            (link_to "Login", new_user_session_path)  
        end 
     end
+
+    def source_helper
+         if session[:source] 
+            content_tag(:p, "Thanks for visiting me from #{session[:source]}", class:"my-class")
+         end
+    end
+
+
 end
  
